7670e420e9a4cd6798f18070a81ca84b86deade8
Volker Mische <volker.mische@gmail.com>
2025-09-16 20:56:20 +0200

    feat: make the DAG-JSON multicodec code public (#15)
    There is no easy way to get the DAG-JSON multicodec code from this library.
    This commits adds a public constant called `DAG_JSON_CODE`.

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,6 @@ mod shared;
 pub use crate::de::{from_reader, from_slice, Deserializer};
 pub use crate::error::{DecodeError, EncodeError};
 pub use crate::ser::{to_vec, to_writer, Serializer};
+
+/// The multicodec code for DAG-JSON.
+pub const DAG_JSON_CODE: u64 = 0x129;
